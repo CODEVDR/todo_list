16fc0e0f8b65d5ed5083be734d27be9d1527e36b
CODEVDR <96605996+CODEVDR@users.noreply.github.com>
2023-06-03 23:23:48 +0530

    main commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+let list = document.getElementById("insert");
+let button = document.getElementById("getTodo");
+button.onclick = () => {
+  let text = document.getElementById("getText");
+  if (text.value === "") {
+    alert("Todo Can't Be Empty");
+  } else {
+    var currentdate = new Date();
+    var datetime =
+      "Last Sync: " +
+      currentdate.getDate() +
+      "/" +
+      (currentdate.getMonth() + 1) +
+      "/" +
+      currentdate.getFullYear() +
+      " - " +
+      currentdate.getHours() +
+      ":" +
+      currentdate.getMinutes() +
+      ":" +
+      currentdate.getSeconds();
+    localStorage.setItem(datetime, text.value);
+    text.innerHTML = null;
+  }
+  list.innerHTML = null;
+  for (let i = 0; i < localStorage.length; i++) {
+    let value = localStorage.getItem(localStorage.key(i));
+    console.info(localStorage.key(i), value);
+    list.innerHTML += `<p class='label' title="${value}">${value}</p>`;
+  }
+};
