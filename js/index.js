4bf46c8a128af55976fd07a8dcc56224b0f87a53
CODEVDR <96605996+CODEVDR@users.noreply.github.com>
2023-06-04 10:54:28 +0530

    main commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
 let list = document.getElementById("insert");
 let button = document.getElementById("getTodo");
+const func = () => {
+  try {
+    list.innerHTML = null;
+    for (let i = 0; i < localStorage.length; i++) {
+      let key = localStorage.key(i);
+      let value = localStorage.getItem(localStorage.key(i));
+      console.info(key, value);
+      // list.innerHTML += `<p class='label' title="${value}">${value}</p>`;
+      list.innerHTML += `<p class="label" title="Stored On ${key}">${value}<i onclick="del('${key}')" title="delete" id="delete" class="fa">&#xf014;</i></p>`;
+    }
+  } catch (error) {}
+};
+func();
+const del = (k) => {
+  localStorage.removeItem(k);
+  func();
+};
 button.onclick = () => {
   let text = document.getElementById("getText");
   if (text.value === "") {
@@ -7,7 +24,6 @@ button.onclick = () => {
   } else {
     var currentdate = new Date();
     var datetime =
-      "Last Sync: " +
       currentdate.getDate() +
       "/" +
       (currentdate.getMonth() + 1) +
@@ -22,12 +38,6 @@ button.onclick = () => {
     localStorage.setItem(datetime, text.value);
     text.innerHTML = null;
   }
-  list.innerHTML = null;
+
+  func();
 };
-try {
-  for (let i = 0; i < localStorage.length; i++) {
-    let value = localStorage.getItem(localStorage.key(i));
-    console.info(localStorage.key(i), value);
-    list.innerHTML += `<p class='label' title="${value}">${value}</p>`;
-  }
-} catch (error) {}
